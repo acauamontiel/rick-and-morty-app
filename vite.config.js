0e1b9b38640a806d0a0bb020ffb68f8fc27a0638
Acauã Montiel <contato@acauamontiel.com.br>
2022-02-20 00:22:58 -0300

    add GitHub CI

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import pugPlugin from 'vite-plugin-pug';
 import {quasar, transformAssetUrls} from '@quasar/vite-plugin';
+import {VitePWA} from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
+	base: '/rick-and-morty-app/',
 	resolve: {
 		alias: {
 			'vue': 'vue/dist/vue.esm-bundler.js',
@@ -16,6 +18,7 @@ export default defineConfig({
 			template: {transformAssetUrls}
 		}),
 		quasar(),
-		pugPlugin()
+		pugPlugin(),
+		VitePWA({})
 	]
 });
